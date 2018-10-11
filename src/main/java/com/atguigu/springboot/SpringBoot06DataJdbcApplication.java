752c9bc599c98b47c7c88a8efc209142298b78d3
huangjianven <huangjianven@126.com>
2018-10-11 11:30:29 +0800

    fd

--- src/main/java/com/atguigu/springboot/SpringBoot06DataJdbcApplication.java
+++ src/main/java/com/atguigu/springboot/SpringBoot06DataJdbcApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBoot06DataJdbcApplication {
-
+//haode
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot06DataJdbcApplication.class, args);
 	}
