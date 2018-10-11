9e453eff43b496c67d008090ac44cdf91ae36096
huangjianven <42291031+huangjianven@users.noreply.github.com>
2018-10-11 11:34:58 +0800

    Update SpringBoot06DataJdbcApplication.java

--- src/main/java/com/atguigu/springboot/SpringBoot06DataJdbcApplication.java
+++ src/main/java/com/atguigu/springboot/SpringBoot06DataJdbcApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBoot06DataJdbcApplication {
-//haode  我是你爸爸
+//haode  我是你爸爸的爹
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot06DataJdbcApplication.class, args);
 	}
